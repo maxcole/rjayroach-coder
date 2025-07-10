080a08efec854ad33c7d1694e9824081588814f8
Robert Roach <rjayroach@gmail.com>
2025-07-10 19:47:27 -0400

    update claude version and avante config

--- dotfiles/nvim/.config/nvim/lua/plugins/avante.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/avante.lua
@@ -7,11 +7,15 @@ return {
   ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
   provider = "claude", -- Recommend using Claude
   auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-  claude = {
-    endpoint = "https://api.anthropic.com",
-    model = "claude-3-5-sonnet-20241022",
-    temperature = 0,
-    max_tokens = 4096,
+  providers = {
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-sonnet-4-20250514",
+      extra_request_body = {
+        max_tokens = 20480,
+        temperature = 0,
+      },
+    },
   },
   ---Specify the special dual_boost mode
   ---1. enabled: Whether to enable dual_boost mode. Default to false.
