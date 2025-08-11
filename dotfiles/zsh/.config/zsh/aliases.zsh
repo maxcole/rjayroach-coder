36f8dff52b00e6b607d6146e547f56d1cb2f8466
Robert Roach <rjayroach@gmail.com>
2025-08-11 11:34:22 -0400

    update terraform/tofu functions

--- dotfiles/zsh/.config/zsh/aliases.zsh
+++ dotfiles/zsh/.config/zsh/aliases.zsh
@@ -18,7 +18,8 @@ clu() {
   if [[ "$1" == "-g" ]]; then
     base_dir="/usr/local"
   fi
-  (cd "${base_dir}/lib/node_modules/@anthropic-ai"; rm -rf claude-code; npm i -g @anthropic-ai/claude-code)
+  rm -rf "${base_dir}/lib/node_modules/@anthropic-ai/claude-code"
+  npm i -g @anthropic-ai/claude-code
 }
 
 alias clv="claude --version"
@@ -129,35 +130,51 @@ tf() {
 
 alias tf-console='tf console'
 alias tf-init='tf init'
-alias tf-list='tf workspace list'
+alias tf-state-list='tf state list'
+alias tf-state-show='tf state show'
+alias tf-workspace-delete='tf workspace delete'
+alias tf-workspace-list='tf workspace list'
+alias tf-workspace-new='tf workspace new'
+alias tf-workspace-select='tf workspace select'
+alias tf-workspace-show='tf workspace show'
+
+tf-apply() { _tf_exec "apply" "$1" }
+tf-apply-auto() { _tf_exec "apply" "$1" "-auto-approve" }
+tf-destroy() { _tf_exec "destroy" "$1" }
+tf-destroy-target() { _tf_exec "destroy" "$1" "-target" "$2"}
+tf-plan() { _tf_exec "plan" "$1" }
 
 _tf_exec() {
   local action="$1"
   local extra_args="$3"  # Add support for extra arguments
   local space
+  local var_file_arg=""
 
   # If parameter provided, use it; otherwise use current workspace
   if [[ -n "$2" ]]; then
     space="$2"
 
-    # Check if tfvars file exists
-    if [[ ! -f "${space}.tfvars" ]]; then
-      echo "Error: ${space}.tfvars file not found"
+    # Switch to the specified workspace
+    if ! tf workspace select ${space}; then
       return 1
     fi
-
-    # Switch to the specified workspace
-    tf workspace select -or-create ${space}
   else
     # Use current workspace
-    space=$(${tf_bin} workspace show)
+    space=$(tf workspace show)
   fi
 
-  # Execute the terraform/tofu command with the appropriate tfvars file
-  tf ${action} -var-file ${space}.tfvars ${extra_args}
-}
+  # Check if tfvars file exists
+  if [[ ! -f "workspace/${space}.tfvars" ]]; then
+    # Require a tfvars file unless its the default workspace
+    if [[ "$space" != "default" ]]; then
+      echo "Error: workspace/${space}.tfvars file not found"
+      return 1
+    fi
+  else
+    # File exists, so include it in the command
+    var_file_arg="-var-file=workspace/${space}.tfvars"
+  fi
 
-tf-apply() { _tf_exec "apply" "$1" }
-tf-apply-auto() { _tf_exec "apply" "$1" "-auto-approve" }
-tf-destroy() { _tf_exec "destroy" "$1" }
-tf-plan() { _tf_exec "plan" "$1" }
+  # Execute the terraform/tofu command
+  tf ${action} ${var_file_arg} ${extra_args}
+}
