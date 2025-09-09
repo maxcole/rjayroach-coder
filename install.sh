a1d3cb7d6576d9cedb9780207e27a242fccd840c
Robert Roach <rjayroach@gmail.com>
2025-09-09 12:08:39 -0400

    move all dev/env packages to this repo

--- install.sh
+++ install.sh
@@ -7,6 +7,17 @@ REPO_URL="git@github.com:maxcole/rjayroach-home.git"
 REPO_DIR=$HOME/rjayroach/home
 SCRIPT_NAME=`basename "$0"`
 
+# Detect the CPU architecture
+detect_arch() {
+  local arch=$(uname -m)
+
+  if [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
+    echo "arm64"
+  else
+    echo "amd64"
+  fi
+}
+
 # Detect the OS
 detect_os() {
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -18,27 +29,76 @@ detect_os() {
   fi
 }
 
+setup_xdg() {
+  mkdir -p $HOME/.cache $HOME/.config $HOME/.local $HOME/.local/share $HOME/.local/state $HOME/.local/bin
+}
+
+
+# Dependencies
+deps_linux() {
+  sudo install -dm 755 /etc/apt/keyrings
+  wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=$(detect_arch)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+  sudo apt update
+
+  # general
+  sudo apt install git neovim stow -y
+
+  # mise
+  sudo apt install curl gpg mise -y
+
+  # tmux
+  sudo apt install entr tmux -y
+  if [ ! -d $HOME/.local/share/tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.local/share/tmux/plugins/tpm
+  fi
+
+  # zsh
+  sudo apt install fzf zsh -y
+  sudo usermod -s /bin/zsh $user
+
+  local omz_dir=$HOME/.local/share/omz
+  if [ ! -d "$omz_dir" ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh.git $omz_dir
+  fi
+  if [ ! -d $omz_dir/custom/plugins/zsh-history-substring-search ]; then
+    git clone https://github.com/zsh-users/zsh-history-substring-search.git $omz_dir/custom/plugins/zsh-history-substring-search
+  fi
+  if [ ! -d $omz_dir/custom/themes/powerlevel10k ]; then
+    git clone https://github.com/romkatv/powerlevel10k.git $omz_dir/custom/themes/powerlevel10k
+  fi
+}
+
+deps_macos() {
+  if ! command -v python3 >/dev/null 2>&1; then
+    debug "ERROR!!"
+    debug ""
+    debug "python interpreter not found. Run 'xcode-select --install' from a terminal then rerun this script"
+    exit 1
+  fi
+}
 
 # Run Once to set the shell, install deps, clone the repo and run the functions
 deps() {
   local user=$(whoami)
 
   if [[ "$(detect_os)" == "linux" ]]; then
-    sudo apt install git stow zsh -y
-    sudo usermod -s /bin/zsh $user
-
+    deps_linux
   elif [[ "$(detect_os)" == "macos" ]]; then
-    if ! command -v python3 >/dev/null 2>&1; then
-      debug "ERROR!!"
-      debug ""
-      debug "python interpreter not found. Run 'xcode-select --install' from a terminal then rerun this script"
-      exit 1
-    fi
+    deps_macos
   fi
 
   if [[ ! -d "$REPO_DIR" ]]; then
     git clone $REPO_URL $REPO_DIR
   fi
+
+  setup_xdg
+}
+
+configure() {
+  nvim --headless "+Lazy! sync" +qa
+  tmux -c $HOME/.local/share/tmux/plugins/tpm/bin/install_plugins
+  tmux -c $HOME/.local/share/tmux/plugins/tpm/tpm
 }
 
 # Create dirs in ~/.config so stow does NOT softlink the entire directory to this repo
