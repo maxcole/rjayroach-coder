d0c97dbf1fde2c2384fdc859805cdccb17ec1dda
Robert Roach <rjayroach@gmail.com>
2025-12-05 17:20:49 +0800

    migrate custom packages to external repo

--- packages/coder/install.sh
+++ packages/coder/install.sh
@@ -1,7 +1,7 @@
 # coder
 
 pre_install() {
-  mkdir -p $XDG_CONFIG_DIR/coder
+  mkdir -p $XDG_CONFIG_DIR/coder $XDG_CONFIG_DIR/tmuxinator
 }
 
 install_linux() { return; }
@@ -10,4 +10,10 @@ install_macos() {
   brew bundle --file=$XDG_CONFIG_DIR/coder/Brewfile
 }
 
-post_install() { return; }
+post_install() {
+  if command -v gem &> /dev/null; then
+    if ! command -v tmuxinator &> /dev/null; then
+      gem install tmuxinator
+    fi
+  fi
+}
